700bc4b99dc8056022c8fe31a9ef2ef5ded8c98a
Diego Mota <diegoalves123@gmail.com>
2019-04-04 19:58:52 -0300

    Salvando novo cliente

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -2,20 +2,20 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from 'rxjs/Rx'; // IMPORTANTE: IMPORT ATUALIZADO
 import { StorageService } from '../services/storage.service';
-import { AlertController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
+import { FieldMessage } from '../models/fieldmessage';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(public storage : StorageService, public alertCtrl : AlertController){
-        
+    constructor(public storage: StorageService, public alertCtrl: AlertController) {
     }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        console.log("Passou no interceptor error");
+        console.log(req);
         return next.handle(req)
         .catch((error, caught) => {
-            
+
             let errorObj = error;
             if (errorObj.error) {
                 errorObj = errorObj.error;
@@ -27,55 +27,80 @@ export class ErrorInterceptor implements HttpInterceptor {
             console.log("Erro detectado pelo interceptor:");
             console.log(errorObj);
 
-            switch(errorObj.status){
+            switch(errorObj.status) {
                 case 401:
-                    this.handle401();
-                case 403:   
-                    this.handle403();
-                    break; 
-                default: 
-                    this.handleDefaultError(errorObj);
-                    break;
+                this.handle401();
+                break;
+
+                case 403:
+                this.handle403();
+                break;
+
+                case 422:
+                this.handle422(errorObj);
+                break;
+
+                default:
+                this.handleDefaultEror(errorObj);
             }
 
             return Observable.throw(errorObj);
         }) as any;
     }
 
-    handle401(){
-       let alert = this.alertCtrl.create({
-           title: 'Erro 401: falha na autenticação',
-           message: "Email ou senha incorretos",
-           enableBackdropDismiss: false, // usado para sair do alert apenas clicando no botão
-           buttons: [
-               {
-                text: 'OK' // texto do botão
-               }
-           ]
-       });
-
-       alert.present(); // exibe o alert
-    }
-
-    handle403(){
+    handle403() {
         this.storage.setLocalUser(null);
     }
 
-    handleDefaultError(errorObj){
+    handle401() {
         let alert = this.alertCtrl.create({
-            title: 'Erro '+ errorObj.status + ':'+ errorObj.error,
-            message: errorObj.message,
-            enableBackdropDismiss: false, // usado para sair do alert apenas clicando no botão
+            title: 'Erro 401: falha de autenticação',
+            message: 'Email ou senha incorretos',
+            enableBackdropDismiss: false,
             buttons: [
                 {
-                 text: 'OK' // texto do botão
+                    text: 'Ok'
                 }
             ]
         });
- 
-        alert.present(); // exibe o alert     
-    }   
+        alert.present();
+    }
 
+    handle422(errorObj) {
+        let alert = this.alertCtrl.create({
+            title: 'Erro 422: Validação',
+            message: this.listErrors(errorObj.erros),
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();
+    }
+
+    handleDefaultEror(errorObj) {
+        let alert = this.alertCtrl.create({
+            title: 'Erro ' + errorObj.status + ': ' + errorObj.error,
+            message: errorObj.message,
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();        
+    }
+
+    private listErrors(messages : FieldMessage[]) : string {
+        let s : string = '';
+        for (var i=0; i<messages.length; i++) {
+            s = s + '<p><strong>' + messages[i].fieldName + "</strong>: " + messages[i].message + '</p>';
+        }
+        return s;
+    }
 }
 
 export const ErrorInterceptorProvider = {
