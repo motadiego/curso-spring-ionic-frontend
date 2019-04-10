78049b60468a6656733e12f377628b642b800488
Diego Mota <diegoalves123@gmail.com>
2019-04-09 22:10:28 -0300

    Terminando as funcionalidades do carrinho

--- src/services/domain/cart.service.ts
+++ src/services/domain/cart.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from "@angular/core";
 import { StorageService } from "../storage.service";
 import { Cart } from "../../models/cart";
 import { ProdutoDTO } from "../../models/produto.dto";
-import { ProdutoDetailPage } from "../../pages/produto-detail/produto-detail";
 
 @Injectable()
 export class CartService{
@@ -37,7 +36,57 @@ export class CartService{
         return cart;
     }
 
+    removeProduto(produto: ProdutoDTO): Cart{
+        let cart = this.getCart();
+        
+        // verifica se o produto está no carrinho
+        let position = cart.items.findIndex(x => x.produto.id == produto.id);
+        if(position != -1){
+            cart.items.splice(position , 1); // remove o produto no carrinho
+        }
+        this.storage.setCart(cart); // atualiza o storage com o carrinho
+        return cart
+    }
 
 
+    increaseQuantity(produto: ProdutoDTO): Cart{
+        let cart = this.getCart();
+        
+        // verifica se o produto está no carrinho
+        let position = cart.items.findIndex(x => x.produto.id == produto.id);
+        if(position != -1){
+            cart.items[position].quantidade++; // incrementwa a quantidade do item (produto) no carrinho
+        }
+        this.storage.setCart(cart); // atualiza o storage com o carrinho
+        return cart
+    }
+
+    
+    decreaseQuantity(produto: ProdutoDTO): Cart{
+        let cart = this.getCart();
+        
+        // verifica se o produto está no carrinho
+        let position = cart.items.findIndex(x => x.produto.id == produto.id);
+        if(position != -1){
+            cart.items[position].quantidade--; // diminui a quantidade do item (produto) no carrinho
+
+            if(cart.items[position].quantidade < 1){
+                cart = this.removeProduto(produto);
+            }
+
+        }
+        this.storage.setCart(cart); // atualiza o storage com o carrinho
+        return cart
+    }
+     
+    total(): number{
+        let cart = this.getCart();
+        let sum = 0;
+        for (var i=0; i < cart.items.length; i++) {
+            let item = cart.items[i];
+            sum +=  item.produto.preco * item.quantidade;
+        }                
+       return sum; 
+    }
 
 }
