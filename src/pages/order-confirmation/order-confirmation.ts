eb6531604e1a8806241262d05985ab7811a6d59b
Diego Mota <diegoalves123@gmail.com>
2019-04-24 21:56:13 -0300

    Salvando pedido

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -6,6 +6,7 @@ import { CartService } from '../../services/domain/cart.service';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { EnderecoDTO } from '../../models/endereco.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
+import { PedidoService } from '../../services/domain/pedido.service';
 
 @IonicPage()
 @Component({
@@ -24,7 +25,8 @@ export class OrderConfirmationPage {
       public navCtrl: NavController, 
       public navParams: NavParams, 
       public cartService: CartService,
-      public clienteService: ClienteService) {
+      public clienteService: ClienteService,
+      public pedidoService: PedidoService) {
     
       this.pedido = this.navParams.get('pedido');  
   }
@@ -51,5 +53,24 @@ export class OrderConfirmationPage {
   total() : number {
     return this.cartService.total();
   } 
+
+
+  /** Metodo usado para inserir um pedido na base de dados */
+  checkout(){
+      this.pedidoService.insert(this.pedido)
+        .subscribe( response => {
+            this.cartService.createOrClearCart();
+            console.log(response.headers.get('location'));
+        },
+         error => {
+            if(error.status == 403){
+              this.navCtrl.setRoot('HomePage');
+            }
+         });
+  }
+  /** Volta para a tela de carrinho */
+  back(){ 
+    this.navCtrl.setRoot('CartPage');
+  }
 
 }
